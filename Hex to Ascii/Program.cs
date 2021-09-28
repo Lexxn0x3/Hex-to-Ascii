dab93c8a1fd135b1b70fea4a5873d25e238c11ea
Robin Rosner <robinrosner@outlook.de>
2021-09-28 09:34:25 +0200

    Add project files.

---
+++ Hex to Ascii/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Hex_to_Ascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string str = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(ConvertHex(str));
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        static string ConvertHex(String hexString)
+        {
+            try
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hexString.Length; i += 2)
+                {
+                    String hs = string.Empty;
+
+                    hs = hexString.Substring(i, 2);
+                    uint decval = System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+
+                }
+
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return string.Empty;
+        }
+    }
+}
